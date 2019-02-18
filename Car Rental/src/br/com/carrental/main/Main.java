028ffcab3819e2751fa65f4bc3ac11255f7aceb8
Bruna Gomes <bruggomes79@gmail.com>
2019-02-18 16:38:53 -0300

    Tratamento de cliente inválido

--- Car Rental/src/br/com/carrental/main/Main.java
+++ Car Rental/src/br/com/carrental/main/Main.java
@@ -10,7 +10,7 @@ import br.com.carrental.rentcalculation.*;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner readInput = new Scanner(System.in);
+		Scanner read = new Scanner(System.in);
 		String line, nameFile;
 		int option, op;
 
@@ -18,21 +18,22 @@ public class Main {
 	    option = new Scanner(System.in).nextInt();
 	    
 	    while(option < 1 || option > 2) {
-			System.out.println("Opção inválida.\"Informe 1 para entrar com os dados diretamente ou 2 para ler de um arquivo de texto:");
-			option = readInput.nextInt();
+			System.out.println("Opção inválida. Informe 1 para entrar com os dados diretamente ou 2 para ler de um arquivo de texto:");
+			option = new Scanner(System.in).nextInt();
 		}
 		  
 	    if(option == 1) {
 			System.out.println("Entre com os dados: \n");
-			line = readInput.nextLine();
+			line = read.nextLine();
 
 			OrganizeData organize = new OrganizeData(line);
 			organize.organizedData(line);
-		
+			
+			
 			//Verifica se a quatidade de passageiros está entro o aceitável
 			while(organize.amount_passenger < 1 || organize.amount_passenger > 7 ) {
 				System.out.println("Dados inválidos. Entre com os dados novamente: \n");
-				line = readInput.nextLine();
+				line = read.nextLine();
 				organize.organizedData(line);
 			}
 			if(organize.client_type.equalsIgnoreCase("normal")) {
@@ -53,16 +54,17 @@ public class Main {
 				System.out.println(client.checkRentalAgency(organize.amount_passenger)); 
 				
 				System.out.println("Pressione 1 para exibir valor das taxas ou 2 para sair");	
-				op = readInput.nextInt();
+				op = new Scanner(System.in).nextInt();
 				
 				if(op == 1) {
 					PremiumClientCalculation clientTwo = new PremiumClientCalculation(organize.amount_passenger);
 					System.out.println("R$" + clientTwo.checkRentalAgency(organize.amount_passenger, organize.calculateRatesWeek(organize.days), organize.calculateRatesWeekend(organize.days)) + ",00");
 				}
-			}
+			}else 
+				System.out.println("Tipo de cliente inválido.");
 		}else if(option == 2)  {
 			System.out.printf("Informe o nome de arquivo texto:\n");
-			nameFile = readInput.nextLine(); 
+			nameFile = read.nextLine(); 
 	    
 			try {
 				FileReader file = new FileReader(nameFile);
@@ -87,7 +89,7 @@ public class Main {
 					System.out.println(client.checkRentalAgency(organize.amount_passenger));
 					
 					System.out.println("Pressione 1 para exibir valor das taxas ou 2 para sair");	
-					op = readInput.nextInt();
+					op = new Scanner(System.in).nextInt();
 					
 					if(op == 1) {						
 						NormalClientCalculation clientTwo = new NormalClientCalculation(organize.amount_passenger);
@@ -99,13 +101,14 @@ public class Main {
 					System.out.println(client.checkRentalAgency(organize.amount_passenger));
 					
 					System.out.println("Pressione 1 para exibir valor ou 2 para sair");	
-					op = readInput.nextInt();
+					op = new Scanner(System.in).nextInt();
 					
 					if(op == 1) {						
 						PremiumClientCalculation clientTwo = new PremiumClientCalculation(organize.amount_passenger);						
 						System.out.println("R$" + clientTwo.checkRentalAgency(organize.amount_passenger, organize.calculateRatesWeek(organize.days), organize.calculateRatesWeekend(organize.days)) + ",00");
 					}
-				}
+				}else 
+					System.out.println("Tipo de cliente inválido.");
 			}catch (IOException e){
 				System.err.printf("Erro na abertura do arquivo: %s.\n",
 				e.getMessage());
